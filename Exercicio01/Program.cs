aba64266d36eec1fe820ef9038d3677667afe009
JoaoVrf13 <joaovictorrfcontato@gmail.com>
2024-09-05 17:57:59 -0300

    Calculo de Retangulo

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Retangulo R = new Retangulo();
+
+            Console.WriteLine("Entre com a largura do retângulo: ");
+            R.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com a altura do retângulo: ");
+            R.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Area: " + R.Area().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Perimetro: " + R.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Diagonal: " + R.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercicio01/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System;
+
+namespace Exercicio01 {
+    internal class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return (2 * Largura) + (2 * Altura);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt ((Largura * Largura) + (Altura * Altura)); 
+           
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
